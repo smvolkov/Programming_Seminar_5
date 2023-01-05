9edfd9755a966a962d2f34a1aca91787d021fbe4
Semion Volkov <semen_volkov@mail.ru>
2023-01-05 11:55:37 +0500

    Task1 added

---
+++ Seminar5.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void RndArray (int Size)
+{
+    int[] array = new int[Size];
+    Random rnd = new Random();
+    int evenCount = 0;
+
+    for (int i = 0; i < Size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        if (array[i] % 2 == 0) evenCount += 1;
+    } 
+
+    Console.WriteLine(String.Join(", ", array));
+    Console.WriteLine(evenCount);
+}
+
+RndArray(4);
